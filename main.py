34beb7007c98339b6e1eb3ca40383d941a185c37
Tianji Rao <rao@TTRao.local>
2023-09-12 17:37:00 -0400

    Change main.py

--- main.py
+++ main.py
@@ -11,5 +11,4 @@ def polars_desc(data):
 
 def polars_visual(data):
     # data = pl.read_csv('Electric_Vehicle_Population_Data.csv')
-    df = pl.read_csv('Electric_Vehicle_Population_Data.csv')
     plt.hist(data["Model Year"])
